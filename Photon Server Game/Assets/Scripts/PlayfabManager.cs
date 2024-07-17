d6dba46cec7a9ea3ed3310c04fa782e962855523
pray0605 <bh0605@naver.com>
2024-07-17 19:45:53 +0900

    (2024.7.16) PlayFab SDK 설치

---
+++ Photon Server Game/Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,63 @@
+using PlayFab;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    [SerializeField] InputField email;
+    [SerializeField] InputField password;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Photon Lobby");
+    }
+
+    public void Success(RegisterPlayFabUserResult result)
+    {
+        Alarm.Show(result.ToString(), AlarmType.Alarm);
+    }
+
+    public void SignUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = email.text,
+            Password = password.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (   
+            request,
+            Success,
+            Failure
+        );
+    }
+    
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email.text,
+            Password = password.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+
+    public void Failure(PlayFabError playFabError)
+    {
+        Alarm.Show(playFabError.ToString(), AlarmType.Alarm);
+    }
+}
